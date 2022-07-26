b5562c18c41b087427c9965e4479180930961731
ismqui <ismqui@gmail.com>
2022-07-26 11:24:18 +0200

    mix test was not working due to environment

--- src/config/test.exs
+++ src/config/test.exs
@@ -16,8 +16,8 @@ import Config
 config :facturacion, Facturacion.Repo,
   # database: "/Users/ismqui/dev/elixir/facturacion_test.db"
   database: Path.expand("../facturacion_test.db", Path.dirname(__ENV__.file)),
-  pool_size: 5,
-  pool: Ecto.Adapters.SQL.Sandbox
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
